4908b7d294cce3c09ee8af11169d281707b4667a
deepavishalig <deepa@nexex.in>
2024-02-20 16:18:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import mindmingle from './img/mindmingle.png';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App" style={{ backgroundImage: `url(${mindmingle})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', height: '100vh', width: '100vw' }}>
+    
+     </div>
+      </div>
+  );
+}
+
+export default App;
